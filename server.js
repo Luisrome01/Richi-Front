3e0713737e0245de8e6024441643b478a167e81d
luisrome <luisrome3005@gmail.com>
2025-01-06 19:56:27 +0100

    pruebalogs

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs'); // Añadido para ver archivos
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,6 +10,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Sirve los archivos estáticos desde la carpeta 'media'
 app.use('/media', express.static(path.join(__dirname, 'media')));
+
+// Ruta para verificar los archivos en 'media'
+app.get('/check-media', (req, res) => {
+    fs.readdir(path.join(__dirname, 'media'), (err, files) => {
+        if (err) {
+            return res.status(500).send('Error reading media folder');
+        }
+        res.send(files); // Muestra los archivos en la carpeta media
+    });
+});
 
 // Ruta para la página principal
 app.get('/', (req, res) => {
