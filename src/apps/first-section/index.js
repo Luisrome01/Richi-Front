0455c5d0df265018d760acaf8ef8a631f6e77ddb
Alejandro Ávila <alejandro.30337890@uru.edu>
2025-01-12 13:49:56 -0400

    feat: crear secciones de páginas como componentes

---
+++ src/apps/first-section/index.js
@@ -0,0 +1,71 @@
+import { VIDEO_EXAMPLE } from '../../utils/media';
+import styles from './style.css?inline';
+
+const css = new CSSStyleSheet();
+css.replaceSync(styles);
+
+const template = document.createElement('template');
+template.innerHTML = /*HTML*/ `
+  <div class="header">
+    <video autoplay loop muted>
+      <source src="${VIDEO_EXAMPLE}" type="video/mp4" />
+      Tu navegador no soporta el formato de video.
+    </video>
+    <div class="waves-container">
+      <svg
+        class="waves"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+        viewBox="0 24 150 28"
+        preserveAspectRatio="none"
+        shape-rendering="auto"
+      >
+        <defs>
+          <path
+            id="gentle-wave"
+            d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
+          />
+        </defs>
+        <g class="parallax">
+          <use
+            xlink:href="#gentle-wave"
+            x="48"
+            y="0"
+            fill="rgba(255,255,255,0.7)"
+          />
+          <use
+            xlink:href="#gentle-wave"
+            x="48"
+            y="3"
+            fill="rgba(255,255,255,0.5)"
+          />
+          <use
+            xlink:href="#gentle-wave"
+            x="48"
+            y="5"
+            fill="rgba(255,255,255,0.3)"
+          />
+          <use xlink:href="#gentle-wave" x="48" y="7" fill="#fff" />
+        </g>
+      </svg>
+    </div>
+  </div>
+  <div class="content flex">
+  </div>
+`;
+
+/**
+ * @description - Sección principal de la página.
+ * @class - FirstSection
+ * @extends HTMLElement
+ */
+class FirstSection extends HTMLElement {
+	constructor() {
+		super();
+		this.appendChild(template.content.cloneNode(true));
+		document.adoptedStyleSheets.push(css);
+	}
+}
+
+customElements.define('first-section', FirstSection);
+export default FirstSection;
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './apps/first-section/index.js';
+
+const app = document.getElementById('app');
+
+app.innerHTML = `
+  <first-section></first-section>
+`;
