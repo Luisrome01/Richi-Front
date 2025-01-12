e52b6ff9f18fe7b34c291b7a602637a32e13c094
Alejandro Ávila <alejandro.30337890@uru.edu>
2025-01-12 13:40:06 -0400

    refactor(utils): corregir nombre asignado a video de ejemplo

--- src/utils/media.js
+++ src/utils/media.js
@@ -3,7 +3,7 @@ const { VITE_IMAGES_ROUTE, VITE_VIDEOS_ROUTE } = import.meta.env;
 export const LOGO_PNG = `${VITE_IMAGES_ROUTE}logoprueba.png`;
 export const LOGO_JPG = `${VITE_IMAGES_ROUTE}logoprueba.jpg`;
 export const BLUE_LOGO = `${VITE_IMAGES_ROUTE}LOGO-2.png`;
-export const VIDEO_EXAMPLE = `${VITE_VIDEOS_ROUTE}videorichi.MP4`;
+export const VIDEO_EXAMPLE = `${VITE_VIDEOS_ROUTE}videorichi.mp4`;
 
 export const mediaMap = {
 	LOGO_PNG,
