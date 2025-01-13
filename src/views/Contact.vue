cc20c983ae47980c0bb9a54ccfe67e6da889f6e9
Alejandro Ávila <alejandro.30337890@uru.edu>
2025-01-13 17:59:34 -0400

    feat(views): crear plantilla base para secciones de la página

---
+++ src/views/Contact.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="contact-container">
+    <div class="background-border"></div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.contact-container {
+  height: 100vh;
+  padding: 33px;
+}
+
+.background-border {
+  position: relative;
+  width: 90vw;
+  height: 761px;
+  background: #f1f1f1;
+  border-radius: 50px;
+}
+</style>
---
+++ src/views/Reviews.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="contact-container"></div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.contact-container {
+  height: 100vh;
+  padding: 33px;
+}
+</style>
