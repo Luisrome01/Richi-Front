18e6c42c7f6994da2a84b1f83743468cad74ef9d
Alejandro Ávila <alejandro.30337890@uru.edu>
2025-01-13 18:00:53 -0400

    feat(src): implementar menú y secciones en archivo raíz del proyecto

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,17 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <Menu />
+  <Welcome />
+  <Tours />
+  <Contact />
+  <Reviews />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script setup>
+import Menu from './components/Menu.vue'
+import Welcome from '@/views/Welcome.vue'
+import Tours from '@/views/Tours.vue'
+import Contact from '@/views/Contact.vue'
+import Reviews from '@/views/Reviews.vue'
+</script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style></style>
