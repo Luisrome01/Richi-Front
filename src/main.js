6cf48d2c95d6718242702f0f03ccd92a43975c9d
Alejandro Ávila <alejandro.30337890@uru.edu>
2025-01-13 12:52:54 -0400

    feat: configurar vue en proyecto

--- src/main.js
+++ src/main.js
@@ -1,29 +1,11 @@
-import './apps/welcome/index.js';
-import './apps/about-us/index.js';
-import './apps/services/index.js';
-import './apps/contact-us/index.js';
-import './apps/photos/index.js';
-import './apps/reviews/index.js';
+import './assets/main.css'
 
-const app = document.getElementById('app');
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
 
-app.innerHTML = /*HTML*/ `
-  <section>
-    <welcome-page></welcome-page>
-  </section>
-  <section id="about-us">
-    <about-us></about-us>
-  </section>
-  <section id="services">
-    <services-page></services-page>
-  </section>
-  <section id="contact-us">
-    <contact-us></contact-us>
-  </section>
-  <section id="photos">
-    <photos-page></photos-page>
-  </section>
-  <section id="reviews">
-    <reviews-page></reviews-page>
-  </section>
-`;
+const app = createApp(App)
+
+app.use(createPinia())
+
+app.mount('#app')
