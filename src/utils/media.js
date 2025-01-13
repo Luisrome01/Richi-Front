8628981955fd979bbcfef2926106e0bdf04f6004
Alejandro Ávila <alejandro.30337890@uru.edu>
2025-01-13 17:57:34 -0400

    feat: importar y registrar nuevas imágenes

--- src/utils/media.js
+++ src/utils/media.js
@@ -1,13 +1,19 @@
-const { VITE_IMAGES_ROUTE, VITE_VIDEOS_ROUTE } = import.meta.env;
+const { VITE_IMAGES_ROUTE, VITE_VIDEOS_ROUTE } = import.meta.env
 
-export const LOGO_PNG = `${VITE_IMAGES_ROUTE}logoprueba.png`;
-export const LOGO_JPG = `${VITE_IMAGES_ROUTE}logoprueba.jpg`;
-export const BLUE_LOGO = `${VITE_IMAGES_ROUTE}LOGO-2.png`;
-export const VIDEO_EXAMPLE = `${VITE_VIDEOS_ROUTE}videorichi.mp4`;
+export const LOGO_PNG = `${VITE_IMAGES_ROUTE}logo.png`
+export const LOGO_JPG = `${VITE_IMAGES_ROUTE}logoprueba.jpg`
+export const BLUE_LOGO = `${VITE_IMAGES_ROUTE}LOGO-2.png`
+export const VIDEO_EXAMPLE = `${VITE_VIDEOS_ROUTE}videorichi.mp4`
+export const WAVES = `${VITE_IMAGES_ROUTE}wave-1.svg`
+export const ABOUT_US = `${VITE_IMAGES_ROUTE}GIF.gif`
+export const ARROW_DIAGONAL = `${VITE_IMAGES_ROUTE}arrow-diagonal.svg`
 
 export const mediaMap = {
-	LOGO_PNG,
-	LOGO_JPG,
-	BLUE_LOGO,
-	VIDEO_EXAMPLE,
-};
+  LOGO_PNG,
+  LOGO_JPG,
+  BLUE_LOGO,
+  VIDEO_EXAMPLE,
+  WAVES,
+  ABOUT_US,
+  ARROW_DIAGONAL,
+}
