aaa58f2d1374867d2356570ede378500f050936d
luisrome <luisrome3005@gmail.com>
2025-01-15 05:03:57 +0100

    input componente

---
+++ src/components/InputGeneral.vue
@@ -0,0 +1,35 @@
+<template>
+  <input
+    :placeholder="placeholder"
+    class="input-general"
+    v-bind="$attrs"
+  />
+</template>
+
+<script>
+export default {
+  name: "InputGeneral",
+  props: {
+    placeholder: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
+
+<style scoped>
+.input-general {
+  border: 1.5px solid #bebebe;
+  border-radius: 100px;
+  background: transparent;
+  padding: 10px 20px;
+  font-size: 16px;
+  color: #292b2e;
+  outline: none;
+}
+
+.input-general::placeholder {
+  color: rgba(41, 43, 46, 0.5);
+}
+</style>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="contact-container">
-    <div class="background-border"></div>
+    <div class="background-border">
+      <InputGeneral placeholder="Escribe aquí..." />
+    </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import InputGeneral from "../components/InputGeneral.vue";
+
+
+</script>
 
 <style scoped>
 .contact-container {
@@ -18,5 +24,8 @@
   height: 761px;
   background: #f1f1f1;
   border-radius: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
