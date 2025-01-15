f7efe843b2509dcc1ebe8c64763a7ec91009ebeb
Alejandro Ávila <alejandro.30337890@uru.edu>
2025-01-14 20:00:11 -0400

    feat: importar y registrar nuevas imágenes para sección de tours

--- src/utils/media.js
+++ src/utils/media.js
@@ -7,6 +7,14 @@ export const VIDEO_EXAMPLE = `${VITE_VIDEOS_ROUTE}videorichi.mp4`
 export const WAVES = `${VITE_IMAGES_ROUTE}wave-1.svg`
 export const ABOUT_US = `${VITE_IMAGES_ROUTE}GIF.gif`
 export const ARROW_DIAGONAL = `${VITE_IMAGES_ROUTE}arrow-diagonal.svg`
+export const VEHICLE = `${VITE_IMAGES_ROUTE}atv-tour-vehicle.png`
+export const DURATION = `${VITE_IMAGES_ROUTE}duration.svg`
+export const CAR = `${VITE_IMAGES_ROUTE}car.svg`
+export const LIST = `${VITE_IMAGES_ROUTE}list.svg`
+export const CAVE = `${VITE_IMAGES_ROUTE}cave-pool.jpg`
+export const CHAPEL = `${VITE_IMAGES_ROUTE}alto-vista.jpg`
+export const LIGHTHOUSE = `${VITE_IMAGES_ROUTE}california.jpg`
+export const LINE = `${VITE_IMAGES_ROUTE}dotted-line.svg`
 
 export const mediaMap = {
   LOGO_PNG,
@@ -16,4 +24,11 @@ export const mediaMap = {
   WAVES,
   ABOUT_US,
   ARROW_DIAGONAL,
+  VEHICLE,
+  DURATION,
+  CAR,
+  LIST,
+  CAVE,
+  CHAPEL,
+  LIGHTHOUSE,
 }
